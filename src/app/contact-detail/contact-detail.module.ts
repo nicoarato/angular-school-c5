f97af1423fc4404170ce8a64c78eeac84d4b5ffb
-KaiK- <enriqueoriol@gmail.com>
2019-05-14 19:05:19 +0200

    updated routes

--- src/app/contact-detail/contact-detail.module.ts
+++ src/app/contact-detail/contact-detail.module.ts
@@ -5,10 +5,12 @@ import { ContactDetailComponent } from './contact-detail.component';
 import { ContactDetailShellComponent } from './contact-detail-shell/contact-detail-shell.component';
 import { ContactDetailResolverService } from './contact-detail-resolver.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 const contactDetailRoutes:Routes = [
     { path: 'contact-detail', component: ContactDetailShellComponent, data:{title: "Contact detail"},
     children: [
+      { path: '', component:ContactFormComponent },
       { path: ':id', component:ContactDetailComponent, 
         resolve:{ contact:ContactDetailResolverService }
       }
@@ -24,7 +26,8 @@ const contactDetailRoutes:Routes = [
     ],
     declarations:[
         ContactDetailComponent,
-        ContactDetailShellComponent
+        ContactDetailShellComponent,
+        ContactFormComponent
     ]
 
 })
